d2fa036584c9ace5bc3396daa1a1b5574145f7b0
ERICK1800 <ericklimaflorencio@gmail.com>
2023-04-03 10:48:10 -0300

    conversores

--- src/app/conversor/conversor.component.ts
+++ src/app/conversor/conversor.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class ConversorComponent {
 
+  num : number = 0;
+  resup : number = 0;
+
+  CMpM(){
+    this.resup = this.num / 100;
+  }
+
+  MpCM(){
+    this.resup = this.num * 100;
+  }
+
+  MpKM(){
+    this.resup = this.num / 1000;
+  }
+
+  KMpM(){
+    this.resup = this.num * 1000;
+  }
+
 }
